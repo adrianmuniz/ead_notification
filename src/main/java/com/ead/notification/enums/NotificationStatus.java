836a873a3eb614d29fcb44a4287d4a01a70c97e7
Cainã Adrian <144962807+PedroVecs@users.noreply.github.com>
2023-10-21 21:23:39 -0300

    Getters And Setter e Enum NotificationStatus

---
+++ src/main/java/com/ead/notification/enums/NotificationStatus.java
@@ -0,0 +1,2 @@
+package com.ead.notification.enums;public enum NotificationStatus {
+}
--- src/main/java/com/ead/notification/models/NotificationModel.java
+++ src/main/java/com/ead/notification/models/NotificationModel.java
@@ -27,4 +27,52 @@ public class NotificationModel implements Serializable {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private NotificationStatus notificationStatus;
+
+    public UUID getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(UUID notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public NotificationStatus getNotificationStatus() {
+        return notificationStatus;
+    }
+
+    public void setNotificationStatus(NotificationStatus notificationStatus) {
+        this.notificationStatus = notificationStatus;
+    }
 }
