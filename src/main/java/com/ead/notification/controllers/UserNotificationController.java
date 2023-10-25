5f776b168fdb7c87e00ab627f4d4ee9a8fc01f16
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-10-25 06:04:16 -0300

    updateNotification

--- src/main/java/com/ead/notification/controllers/UserNotificationController.java
+++ src/main/java/com/ead/notification/controllers/UserNotificationController.java
@@ -1,5 +1,6 @@
 package com.ead.notification.controllers;
 
+import com.ead.notification.dtos.NotificationDto;
 import com.ead.notification.models.NotificationModel;
 import com.ead.notification.services.NotificationService;
 import org.springframework.data.domain.Page;
@@ -8,11 +9,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -29,5 +29,19 @@ public class UserNotificationController {
     public ResponseEntity<Page<NotificationModel>> getAllNotificationsByUser(@PathVariable(value="userId") UUID userId,
                                                                              @PageableDefault(page = 0, size = 10, sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(notificationService.findAllNotificationsByUser(userId, pageable));
+    }
+
+    @PutMapping("/users/{userId}/notifications/{notificationId}")
+    public ResponseEntity<Object> updateNotification(@PathVariable(value="userId") UUID userId,
+                                                     @PathVariable(value="notificationId") UUID notificationId,
+                                                     @RequestBody @Valid NotificationDto notificationDto){
+        Optional<NotificationModel> notificationModelOptional =
+                notificationService.findByNotificationIdAndUserId(notificationId, userId);
+        if(notificationModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Notification not found!");
+        }
+        notificationModelOptional.get().setNotificationStatus(notificationDto.getNotificationStatus());
+        notificationService.saveNotification(notificationModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body(notificationModelOptional.get());
     }
 }
