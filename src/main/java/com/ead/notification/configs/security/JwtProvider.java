4019bc18443ff2ef2bfc0dabff0e6ef7e78a430f
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-12-08 00:22:42 -0300

    Access Token Pattern in Notification Microservice

--- src/main/java/com/ead/notification/configs/security/JwtProvider.java
+++ src/main/java/com/ead/notification/configs/security/JwtProvider.java
@@ -40,7 +40,4 @@ public class JwtProvider {
         }
         return false;
     }
-
-
-
 }
--- src/main/java/com/ead/notification/controllers/UserNotificationController.java
+++ src/main/java/com/ead/notification/controllers/UserNotificationController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,7 +28,8 @@ public class UserNotificationController {
 
     @GetMapping("/users/{userId}/notifications")
     public ResponseEntity<Page<NotificationModel>> getAllNotificationsByUser(@PathVariable(value="userId") UUID userId,
-                                                                             @PageableDefault(page = 0, size = 10, sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable){
+                                                                             @PageableDefault(page = 0, size = 10, sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                                             Authentication Authentication){
         return ResponseEntity.status(HttpStatus.OK).body(notificationService.findAllNotificationsByUser(userId, pageable));
     }
 
